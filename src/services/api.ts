f5a9571070c0bf92663ebbdd25838fc726dbaf9b
erase2004 <erase2004@gmail.com>
2022-04-27 16:24:11 +0800

    [Add] constant for TAG_SEPERATOR

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import { apiHelper } from "@/utils/helpers";
 import { TagList } from '@/types/share'
+import { TAG_SEPERATOR } from "@/constants/share";
 
 const tagCount = 10
 const questionCount = 20
@@ -15,13 +16,14 @@ export default {
       }
     })
   },
-  getQeustionsByTag(tagList: TagList) {
+  getQeustionsByTag(tagList: TagList, page: number) {
     return apiHelper.get('/questions', {
       params: {
+        page,
         pagesize: questionCount,
         order: 'desc',
         sort: 'votes',
-        tagged: tagList.join(';'),
+        tagged: tagList.join(TAG_SEPERATOR),
         site: 'stackoverflow'
       }
     })
