2797c5309cb10538fa431987689e9f04eb130781
erase2004 <erase2004@gmail.com>
2022-04-27 12:28:47 +0800

    [Add] Trending List

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <Search></Search>
+  <TrendingList></TrendingList>
 </template>
 
 <script setup lang="ts">
 import Search from '@/components/Search.vue';
+import TrendingList from '@/components/TrendingList.vue';
 
 </script>
