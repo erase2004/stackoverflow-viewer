199f33868a56b53d752e7360c3e3f47d52c5818c
erase2004 <erase2004@gmail.com>
2022-04-27 19:29:18 +0800

    [Deprecate] Remove pinia since not in use

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import '@/styles/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createPinia } from 'pinia'
 
 const app = createApp(App)
-app.use(createPinia())
 app.mount('#app')
