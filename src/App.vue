6feaaefdf09345c6ce37451e83f01e3372180445
erase2004 <erase2004@gmail.com>
2022-04-27 12:30:01 +0800

    [Add] Question List

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <Search></Search>
   <TrendingList></TrendingList>
+  <QuestionList></QuestionList>
 </template>
 
 <script setup lang="ts">
 import Search from '@/components/Search.vue';
 import TrendingList from '@/components/TrendingList.vue';
+import QuestionList from '@/components/QuestionList.vue'
 
 </script>
